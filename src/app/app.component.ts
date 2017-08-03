12384fb0bef35c65f3da47733fc494c4c6af8d8f
Manivannan <manivannan.baskaran@accenture.com>
2017-08-03 10:21:44 +0530

    ngClassDemo

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  template: `
+        <h1>ngClass demo</h1>
+          <div [ngClass]="{bold:isBold, strike:isStrike, highlight:isHighlight}">
+            Hello, NgMigrate!!
+          </div>
+          <hr/>
+          <div>
+            <h3>Input via checkbox</h3>
+            <p>
+              Click each of the checkboxes to see the effect
+            </p>
+            <label><input type="checkbox" [(ngModel)]="isBold">Bold</label>
+            <label><input type="checkbox" [(ngModel)]="isStrike">Strike</label>
+            <label><input type="checkbox" [(ngModel)]="isHighlight">Highlight</label>
+          </div>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    isBold = false;
+    isStrike = false;
+    isHighlight = false;
+
+
+}
